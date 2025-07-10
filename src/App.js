3778373a273428c8105a9836196e63655f059eaf
Tran Vu Quyet <quyettranvu@gmail.com>
2025-07-11 01:31:22 +0700

    trigger CI/CD

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
           <a href="/students" className="navbar-brand">
-            Student Management APP
+            Student Management APP triggered by CodePipeline
           </a>
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
